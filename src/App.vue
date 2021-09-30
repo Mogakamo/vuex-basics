1a027e00703f0b347476139dc50d95bb064a51a5
EC2 Default User <ec2-user@ip-172-31-41-174.us-east-2.compute.internal>
2021-09-30 08:48:49 +0000

    working on the vuex store

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="container">
+    <Todo />  
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>Todos</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Todo',
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    
+</style>
